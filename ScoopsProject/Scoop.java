b36b6c59703c7941b7c103999f90fdd4cf522272
Zhi Shen Yong <zyong@wustl.edu>
2018-08-28 19:36:18 -0500

    scoop stuff

--- ScoopsProject/Scoop.java
+++ ScoopsProject/Scoop.java
@@ -1,5 +1,17 @@
 package ScoopsProject;
 
+import java.
+
 public class Scoop {
+	
+	// Each scoop has the same radius
+	public int scoopRadius = 50;
+	
+	// Object constructor
+	// Takes in the width of the screen to randomly generate
+	// spawning location
+	public Scoop(int screenWidth) {
+		
+	}
 
 }
