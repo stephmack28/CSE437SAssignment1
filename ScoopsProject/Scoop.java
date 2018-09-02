b516972e7ca7778af3a05bf1238da1cc32bdb5cc
Zhi Shen Yong <zyong@wustl.edu>
2018-09-02 11:56:10 -0500

    scoop javadoc

--- ScoopsProject/Scoop.java
+++ ScoopsProject/Scoop.java
@@ -24,57 +24,81 @@ public class Scoop {
 		color = c;
 	}
 
-	// Method to move scoops
-	//		Takes in a number of pixels to jump (so scoop falling speed can be increased/decreased)
-	// 		Upon a successful jump, return true
-	// 		If the scoop touches the bottom of the screen, return false
-
 	/**
 	 * Called each time a scoop needs to be moved down the screen
-	 * @param pixelsToJump
+	 * @param pixelsToJump An int representing how many pixels a scoop should be moved
 	 */
 	public void moveScoop(int pixelsToJump) {
 		y = y - pixelsToJump;
 	}
 
-	// Method to check if scoop has touched the ground
+	/**
+	 * A method to check if a scoop has touched the ground
+	 * @return A boolean for if the scoop has touched the ground
+	 */
 	public boolean isScoopOnGround() {
-		if (y < (scoopRadius - 35)) {
-			return true;
-		} else {
-			return false;
+		if (scored == false) {
+			if (y < (scoopRadius - 35)) {
+				return true;
+			} else {
+				return false;
+			}
 		}
+		return false;
 	}
 
-	// Method to return radius of scoop
+	/**
+	 * A method to return the radius of the scoop
+	 * @return An int representing the radius of the scoop
+	 */
 	public int getRadius() {
 		return scoopRadius;
 	}
 
-	// Method to return x coordinate of scoop
+	/**
+	 * A method to return the x coordinate of scoop
+	 * @return An int representing the x coordinate of the scoop
+	 */
 	public int getX() {
 		return x;
 	}
 
-	// Method to return y coordinate of scoop
+	/**
+	 * A method to return the y coordinate of the scoop
+	 * @return An int representing the y coordinate of the scoop
+	 */
 	public int getY() {
 		return y;
 	}
 
-	//Method to set color
+	/**
+	 * A method to set the color of the scoop
+	 * @param c A Color to set the color of the scoop as
+	 */
 	public void setColor(Color c) {
 		color = c;
 	}
 
-	//Method to get color
+	/**
+	 * A method to return the color of the scoop
+	 * @return A Color representing the color of the scoop
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * A method to set the scored state of the scoop
+	 * @param b A boolean representing if the scoop has been scored or not
+	 */
 	public void setScored(boolean b) {
 		scored = b;
 	}
 
+	/**
+	 * A method to return the scored state of the scoop
+	 * @return A boolean representing the scored state of the scoop
+	 */
 	public boolean getScored() {
 		return scored;
 	}
