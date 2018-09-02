8546b484952bd2a6ec7de80624daf9c725888be7
Zhi Shen Yong <zyong@wustl.edu>
2018-09-02 10:31:01 -0500

    javadocs documentation 1

--- ScoopsProject/Scoop.java
+++ ScoopsProject/Scoop.java
@@ -11,10 +11,12 @@ public class Scoop {
 	private Color color;
 	private boolean scored;
 
-	// Object constructor
-	//		Takes in the height and width of the screen
-	// 		Randomly determines a horizontal coordinate for the scoop
-	// 		Scoops spawn just above the top of the screen
+	/**
+	 * Represents a scoop object
+	 * @param screenHeight An int representing the height of the game window
+	 * @param screenWidth An int representing the width of the game window
+	 * @param c A Color representing the color of the scoop
+	 */
 	public Scoop(int screenHeight, int screenWidth, Color c) {
 		double rand = Math.random();
 		x = (int) ((screenWidth - (scoopRadius * 2)) * rand);
@@ -26,6 +28,11 @@ public class Scoop {
 	//		Takes in a number of pixels to jump (so scoop falling speed can be increased/decreased)
 	// 		Upon a successful jump, return true
 	// 		If the scoop touches the bottom of the screen, return false
+
+	/**
+	 * Called each time a scoop needs to be moved down the screen
+	 * @param pixelsToJump
+	 */
 	public void moveScoop(int pixelsToJump) {
 		y = y - pixelsToJump;
 	}
